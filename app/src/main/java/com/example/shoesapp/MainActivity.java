d02b1b09eff32fcd621203ffca377ef357f41753
Dhruv <dhruvgajera50@gmail.com>
2024-07-20 15:17:01 +0530

    Final Commit

--- app/src/main/java/com/example/shoesapp/MainActivity.java
+++ app/src/main/java/com/example/shoesapp/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.shoesapp.databinding.ActivityLoginBinding;
 import com.example.shoesapp.databinding.ActivityMainBinding;
@@ -22,8 +25,29 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
 
+            if (itemId == R.id.home){
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.order){
+                replaceFragment(new OrderFragment());
+            } else if (itemId == R.id.profile){
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+
+    }
+
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 
 
